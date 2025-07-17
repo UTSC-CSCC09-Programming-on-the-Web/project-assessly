9d314b6be2e574cb0e47486eadebf06eedf32f8b
AhmadHakim2004 <hakimahmad2004@gmail.com>
2025-07-17 10:59:07 -0400

    Implemented and integrated deletion and added assignment model

---
+++ backend/models/assignments_model.ts
@@ -0,0 +1,22 @@
+import { sequelize } from '../datasource.js';
+import { DataTypes } from 'sequelize';
+import { AssessmentModel } from './assessments_model.js';
+
+export const AssignmentModel = sequelize.define('Assignment', {
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    is_completed: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false
+    },
+    grade: {
+        type: DataTypes.FLOAT,
+        allowNull: true,
+    },
+});
+
+AssignmentModel.belongsTo(AssessmentModel);
+AssessmentModel.hasMany(AssignmentModel);
