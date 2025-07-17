47816ab6c1ebc51813a36b60a42fabbaa029d41c
AhmadHakim2004 <hakimahmad2004@gmail.com>
2025-07-17 02:36:59 -0400

    Implement frontend for HomeView

---
+++ frontend/src/components/GoogleSignInButton.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { useRouter } from 'vue-router';
+const router = useRouter();
+
+async function onSigninClick() {
+    router.push('/candidate-dashboard');
+}
+</script>
+
+<template>
+	<button
+		class="inline-flex items-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 transition-colors duration-200"
+		@click="onSigninClick"
+		>
+		Sign in with Google
+	</button>
+</template>
